95f935dfdb9bd9086a9f2951d6c4932d0c1b6269
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2022-11-15 08:03:49 +0530

    testing python scripts

---
+++ idk.py
@@ -0,0 +1,18 @@
+# import mysql.connector 
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="Dineth",
+#   password="1234",
+#   database = "dinemate"
+# )
+# mycursor = mydb.cursor()
+# mycursor.execute("SHOW TABLES")
+
+
+# l = []
+# for x in mycursor:
+#     l.append(x)
+#     # print(x)
+# print(l)
+
+print("TESTING")
--- securepage.php
+++ securepage.php
@@ -10,17 +10,30 @@ require_once 'includes/auth/authcheck.inc.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        a{
+            margin: 10px;
+            padding: 10px;
+            display: block;
+        }
+    </style>
 </head>
 
 <body>
     <br>
-    Hi there this can only be accessed if logged in
+    <h1>Hi there this can only be accessed if logged in</h1>
     <br>
     <a href="index.php"> Go back to index </a>
     <br>
     <a href="NewDish.php"> Add a new dish </a>
     <br>
     <a href="ViewDishes.php"> View all dishes </a>
+
+    <?php
+    $output = shell_exec('idk.py');
+    echo "<div>".$output."</div>";
+
+    ?>
 
 </body>
 
